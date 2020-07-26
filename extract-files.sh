8639240c8c844e7e814e23e2a5b0b6a2c07e850e
filiprrs <filiprrs@gmail.com>
2020-07-27 00:07:05 +0200

    a5y17lte: Point to common 7880 extract script
    Device-specific script was only used for RIL. But we now pull those blobs from a common source.
    
    Change-Id: I0ae03f4280f16f6ef71892b7cd5d0563ab42ade1

--- extract-files.sh
+++ extract-files.sh
@@ -14,43 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
-VENDOR=samsung
-DEVICE=a5y17lte
-
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" true
-
-extract "$MY_DIR"/proprietary-files.txt "$SRC"
-
-"$MY_DIR"/setup-makefiles.sh
+./../universal7880-common/extract-files.sh $@
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,32 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-
-VENDOR=samsung
-DEVICE=a5y17lte
-
-# Load extractutils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-LINEAGE_ROOT="$MY_DIR"/../../..
-
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-# The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# Done
-write_footers
+./../universal7880-common/setup-makefiles.sh $@
